fda55094d25e219be291ce407fbde9d34f10db7c
suhani013 <122560290+suhani013@users.noreply.github.com>
2023-01-30 13:26:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
+function update_scoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("player A: " + player_a)
+    OLED.newLine()
+    OLED.writeStringNewLine("player B: " + player_b)
+    OLED.newLine()
+    OLED.writeStringNewLine("ties: " + ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("rounds " + rounds)
+}
 input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
 	
 })
+let rounds = 0
+let player_a = 0
+let player_b = 0
+let ties = 0
 OLED.init(128, 64)
 OLED.writeStringNewLine("New Game Loaded")
-let ties = 0
-let player_b = 0
-let player_a = 0
-let rounds = 0
+ties = 0
+player_b = 0
+player_a = 0
+rounds = 0
+basic.pause(2000)
+update_scoreboard()
