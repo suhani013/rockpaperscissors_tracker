1187cd8a523c4cbc7463a954aadc1433213bb305
suhani013 <122560290+suhani013@users.noreply.github.com>
2023-01-31 13:44:57 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 def on_button_pressed_a():
-    pass
+    global player_a, rounds
+    basic.show_string("A")
+    basic.clear_screen()
+    player_a += 1
+    rounds += 1
+    update_scoreboard()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def update_scoreboard():
+    OLED.clear()
+    OLED.write_string_new_line("player A: " + str(player_a))
+    OLED.new_line()
+    OLED.write_string_new_line("player B: " + str(player_b))
+    OLED.new_line()
+    OLED.write_string_new_line("ties: " + str(ties))
+    OLED.new_line()
+    OLED.write_string_new_line("rounds " + str(rounds))
+
 def on_button_pressed_ab():
-    pass
+    global rounds
+    basic.show_string("T")
+    basic.clear_screen()
+    rounds += 1
+    update_scoreboard()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    pass
+    global player_b, rounds
+    basic.show_string("B")
+    basic.clear_screen()
+    player_b += 1
+    rounds += 1
+    update_scoreboard()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-OLED.init(128, 64)
-OLED.write_string_new_line("New Game Loaded")
+def on_gesture_shake():
+    reset()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def reset():
+    global ties, player_b, player_a, rounds
+    OLED.init(128, 64)
+    OLED.write_string_new_line("New Game Loaded")
+    ties = 0
+    player_b = 0
+    player_a = 0
+    rounds = 0
+    basic.pause(2000)
+    update_scoreboard()
 ties = 0
 player_b = 0
-player_a = 0
 rounds = 0
+player_a = 0
+reset()
--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showString("A")
+    basic.clearScreen()
+    player_a += 1
+    rounds += 1
+    update_scoreboard()
 })
+function declare_winner () {
+    if (rounds == 20) {
+        if (player_a < player_b) {
+            OLED.clear()
+            OLED.writeStringNewLine("Player B Wins")
+        }
+        if (player_b < player_a) {
+            OLED.clear()
+            OLED.writeStringNewLine("Player A Wins")
+        }
+        if (player_b == player_a) {
+            OLED.clear()
+            OLED.writeStringNewLine("Tie")
+        }
+        basic.pause(5000)
+        reset()
+    }
+}
 function update_scoreboard () {
     OLED.clear()
     OLED.writeStringNewLine("player A: " + player_a)
@@ -10,22 +32,37 @@ function update_scoreboard () {
     OLED.writeStringNewLine("ties: " + ties)
     OLED.newLine()
     OLED.writeStringNewLine("rounds " + rounds)
+    declare_winner()
 }
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showString("T")
+    basic.clearScreen()
+    rounds += 1
+    ties += 1
+    update_scoreboard()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showString("B")
+    basic.clearScreen()
+    player_b += 1
+    rounds += 1
+    update_scoreboard()
 })
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("New Game Loaded")
+    ties = 0
+    player_b = 0
+    player_a = 0
+    rounds = 0
+    basic.pause(2000)
+    update_scoreboard()
+}
+let ties = 0
+let player_b = 0
 let rounds = 0
 let player_a = 0
-let player_b = 0
-let ties = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("New Game Loaded")
-ties = 0
-player_b = 0
-player_a = 0
-rounds = 0
-basic.pause(2000)
-update_scoreboard()
+reset()
